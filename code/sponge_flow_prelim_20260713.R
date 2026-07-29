9a3e3ad43985fd44c877e9028ae11ed001c32d3e
vlxlu <vanessalx.lu@mail.utoronto.ca>
2026-07-29 12:54:58 -0700

    reading in updated raw data files (29 July)

---
+++ code/sponge_flow_prelim_20260713.R
@@ -0,0 +1,147 @@
+library(dplyr)
+library(ggplot2)
+#Things to potentially flag:
+#CA_S5_01 puff #4 is lagging edge
+#DA_S35_02 puff #3 is lagging edge
+#CA)S62)_O1 puff #5 is short (cut?)
+
+sponge_flow <- read.csv("sponge_tracks_26June2026.csv")
+sponge_flow <- sponge_flow %>%
+  mutate(across(c(t, x, y, r, v), as.numeric))
+
+speed_perpuff <- sponge_flow %>%
+  group_by(osculum_ID, tracking_num) %>%
+  filter(osculum_ID != "") %>%
+  summarise(
+    abs_distance_cm = abs(last(r) - first(r)),
+    time_elapsed_s  = last(t) - first(t),
+    speed_cm_s      = abs_distance_cm / time_elapsed_s,
+    .groups = "drop"
+  )
+print(speed_perpuff, n = Inf)
+  #What is up with AB_S26_O3     puff        6? why so slow?
+
+#Here, what is we trim to only keep first 2 cm max. This will make videos approximately hte same 
+#length
+speed_perpuff_2cm <- sponge_flow %>%
+  filter(osculum_ID != "") %>%
+  arrange(osculum_ID, tracking_num, t) %>%
+  group_by(osculum_ID, tracking_num) %>%
+  filter(r <= first(r) + 2) %>%
+  summarise(
+    abs_distance_cm = abs(last(r) - first(r)),
+    time_elapsed_s  = last(t) - first(t),
+    speed_cm_s      = abs_distance_cm / time_elapsed_s,
+    .groups = "drop"
+  )
+
+print(speed_perpuff_2cm, n=Inf)
+
+
+#Correlation
+cor(speed_perpuff_2cm$speed_cm_s, speed_perpuff$speed_cm_s )
+plot(speed_perpuff_2cm$speed_cm_s, speed_perpuff$speed_cm_s )
+
+#Calculate mean per osculum
+speed_mean <- speed_perpuff_2cm %>%
+  group_by(osculum_ID) %>%
+  summarise(
+    mean_speed = mean(speed_cm_s, na.rm = TRUE),
+    sd_speed   = sd(speed_cm_s, na.rm = TRUE),
+    .groups = "drop"
+  )  
+print(speed_mean, n = Inf)
+
+
+## Read in sponge morphology data set
+sponge_size <- read.csv("sponge_size_data.csv")
+
+
+#Join sponge size data for matching sponges
+speed_mean <- speed_mean %>%
+  left_join(sponge_size, by = c("osculum_ID" = "id"))
+
+ggplot(speed_mean, aes(x = species, y = mean_speed)) +
+  geom_boxplot() +
+  labs(x = "Species", y = "Mean speed (cm/s)") +
+  theme_classic()
+
+#So far, particle velocity not sig different across three species
+model <- aov(mean_speed ~ species, data = speed_mean)
+summary(model)
+
+#Ok, let's look at flow rate...this requires different columns for different species
+#becaues they have different shapes.
+
+speed_mean <- speed_mean %>%
+  mutate(
+    osc_diam1_cm = as.numeric(na_if(as.character(osc_diam1_cm), ".")),
+    osc_diam2_cm = as.numeric(na_if(as.character(osc_diam2_cm), "."))
+  )
+
+#first calculate cross sectional area
+speed_mean <- speed_mean %>%
+  mutate(
+    cross_sec_area = case_when(
+      species %in% c("Archeri", "Lacunosa") ~ pi * (osc_diam1_cm/2)^2,
+      species == "Touchmenot"               ~ pi * (osc_diam1_cm/2) * (osc_diam2_cm/2)
+    )
+  )
+ggplot(speed_mean, aes(x = species, y = cross_sec_area)) +
+  geom_boxplot() +
+  labs(x = "Species", y = "Cross-sectional area (cm²)") +
+  theme_classic()
+
+#Now calculate flow, make box plot, and run anova
+speed_mean <- speed_mean %>%
+  mutate(osc_flow = cross_sec_area * mean_speed)
+ggplot(speed_mean, aes(x = species, y = osc_flow)) +
+  geom_boxplot() +
+  labs(x = "Species", y = "Osculum flow (cm³/s)") +
+  theme_classic()
+
+model_flow <- aov(osc_flow ~ species, data = speed_mean)
+summary(model_flow)
+TukeyHSD(model_flow)
+##tmn is significantly higher than other two
+
+#Think about applying correction of 0.5...look for old papers....
+#we will also want to emphasize that this is about RELATIVE speed, not necessarily
+#getting perfect measurement of flow!
+
+#For TMN...want to look at flow and how it (maybe) impacts tail beat freq of fish
+
+#read in fish data
+fish_tbf <- read.csv("tbf_correlates.csv")
+str(fish_tbf)
+fish_tbf <- fish_tbf  %>%
+  mutate(across(c(diam1, diam2, osc_depth, osculum_vol, max_height_cm, tbf_mean, depth), 
+                ~as.numeric(na_if(., "."))))
+
+fish_tbf$Fish.Size <- factor(fish_tbf$Fish.Size, levels = c("Small", "Medium", "Large"))
+
+#Join rows that match from the flow and fish data (so far there are 20 vids)
+TMN_complete <- speed_mean %>%
+  inner_join(fish_tbf, by = c("osculum_ID" = "Sponge"))
+print(TMN_complete, n=Inf)
+
+
+#Let's see if we can't recreate Basma's finding of marg effect of fish size (qualitative)
+#on tbf
+lm_size <- lm(TMN_complete$tbf_first ~ TMN_complete$Fish.Size)
+summary(lm_size)
+
+#Same trend...see if we can boost sample size a bit more
+plot(TMN_complete$Fish.Size, TMN_complete$tbf_first)
+
+#now try flow rate (nope!)
+lm_flow <- lm( TMN_complete$tbf_first ~ TMN_complete$osc_flow)
+summary(lm_flow)
+plot(TMN_complete$osc_flow, TMN_complete$tbf_first)
+
+#Ok, look at interaction just in case
+lm_interac <- lm( TMN_complete$tbf_first ~ TMN_complete$osc_flow*TMN_complete$Fish.Size)
+summary(lm_interac)
+#nope...it's just size
+
+
